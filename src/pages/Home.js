6f5f970428567d484605c356f7e3500fa6516477
armandokanafani <armandokanafani@gmail.com>
2023-01-14 23:40:34 +0700

    add skill section (home page)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import { Player } from '@lottiefiles/react-lottie-player'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faCss3Alt, faJs, faPhp, faJava, faBootstrap, faLaravel, faReact} from '@fortawesome/free-brands-svg-icons'
 
 const Home = () => {
     return(
@@ -17,10 +19,76 @@ const Home = () => {
             {/* Hero Section End */}
 
             {/* Languange Setion Start */}
-            <section>
+            <section className='flex flex-col px-12 md:px-24 lg:px-36'>
+                <h1 className='font-bold text-2xl text-center lg:text-4xl '>Programming</h1>
+
+                {/* Icon Programming Area*/}
+                <div className='flex flex-wrap lg:px-36 justify-center border drop-shadow-lg gap-4 w-full my-6 mb-12 self-center py-6 px-6 rounded-lg'>
+                    
+                    {/* Html icon*/}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-orange-600' icon={faHtml5} />
+                        <span className='text-sm font-medium px-2'>HTML</span>
+                    </div>
+
+                    {/* CSS icon*/}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-blue-700' icon={faCss3Alt} />
+                        <span className='text-sm font-medium px-2'>CSS</span>
+                    </div>
+
+                    {/* Javascript icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-yellow-400' icon={faJs} />
+                        <span className='text-sm font-medium px-2'>Javascript</span>
+                    </div>
+
+                    {/* PHP icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-blue-400' icon={faPhp} />
+                        <span className='text-sm font-medium px-2'>PHP</span>
+                    </div>
+
+                    {/* Java icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-blue-500' icon={faJava} />
+                        <span className='text-sm font-medium px-2'>Java</span>
+                    </div>
+
+                    {/* Bootstrap icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-purple-600' icon={faBootstrap} />
+                        <span className='text-sm font-medium px-2'>Bootstrap</span>
+                    </div>
+
+                    {/* Laravel icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-red-700' icon={faLaravel} />
+                        <span className='text-sm font-medium px-2'>Laravel</span>
+                    </div>
+
+                    {/* Tailwind icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-blue-500' icon={faCss3Alt} />
+                        <span className='text-sm font-medium px-2'>Tailwinds</span>
+                    </div>
+
+                    {/* React icon */}
+                    <div className='bg-gray-300 rounded-lg px-2 py-px'>
+                        <FontAwesomeIcon className='text-blue-300' icon={faReact} />
+                        <span className='text-sm font-medium px-2'>React Js</span>
+                    </div>
+                </div>
 
             </section>
             {/* Languange Section End   */}
+
+            {/* Experiance Section Start */}
+            <section>
+                <h1>Experiance</h1>
+                
+            </section>
+            {/* Experiance Section End */}
         </div>
 
     )
